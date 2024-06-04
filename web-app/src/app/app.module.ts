6ec4209aa3e6b10c02ca147145dc17bab52f5c03
Eros Nolasco-Mendoza <erosnolasco@me.com>
2024-06-04 16:17:32 -0700

    Added simple sign up page

--- web-app/src/app/app.module.ts
+++ web-app/src/app/app.module.ts
@@ -17,7 +17,7 @@ import { SharedModule } from './shared/shared.module';
     AppRoutingModule,
     BrowserAnimationsModule,
     CoreModule,
-    SharedModule
+    SharedModule,
 
   ],
   providers: [],
