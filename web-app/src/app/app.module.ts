ccef0f44ca0867926ce53b1ac17584c7fac2692e
Eros Nolasco-Mendoza <erosnolasco@me.com>
2024-06-04 18:15:19 -0700

    fixed routing

--- web-app/src/app/app.module.ts
+++ web-app/src/app/app.module.ts
@@ -1,11 +1,12 @@
 import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { CoreModule } from './core/core.module';
 import { SharedModule } from './shared/shared.module';
+import { RouterModule } from '@angular/router';
 
 
 @NgModule({
@@ -14,10 +15,10 @@ import { SharedModule } from './shared/shared.module';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
     BrowserAnimationsModule,
     CoreModule,
     SharedModule,
+    RouterModule,
 
   ],
   providers: [],
--- web-app/src/app/core/core.module.ts
+++ web-app/src/app/core/core.module.ts
@@ -4,12 +4,16 @@ import { SharedModule } from '../shared/shared.module';
 import { LandingPageComponent } from './landing-page/landing-page.component';
 import { SignUpComponent } from './sign-up/sign-up.component';
 import { SignInComponent } from './sign-in/sign-in.component';
+import { AppRoutingModule } from './app-routing.module';
+
 
 @NgModule({
   declarations: [LandingPageComponent, SignUpComponent, SignInComponent],
   imports: [
     CommonModule,
-    SharedModule
+    SharedModule,
+    AppRoutingModule
+    
   ],
   exports: [
     LandingPageComponent, SignUpComponent, SignInComponent
